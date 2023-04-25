c031ced16da368b454735ed9eb211e1e64da7288
Arkadiy Sychkov <Z4mmer@gmail.com>
2023-04-26 03:55:19 +0500

    Поправил вторую функцию (две реализации)

--- HomeWork_QA_GURU_5_6.py
+++ HomeWork_QA_GURU_5_6.py
@@ -21,10 +21,23 @@ def test_dark_theme_by_time_and_user_choice():
     dark_theme_enabled_by_user = False - Темная тема выключена
     dark_theme_enabled_by_user = None - Пользователь не сделал выбор (используется переключение по времени системы)
     """
-    current_time = time(hour=16)
+    current_time = time(hour=23)
     dark_theme_enabled_by_user = None
     # TODO переключите темную тему в зависимости от времени суток,
     #  но учтите что темная тема может быть включена вручную
+
+    if dark_theme_enabled_by_user is None and current_time.hour >= 22 or current_time.hour <= 6:
+        is_dark_theme = True
+    elif dark_theme_enabled_by_user is True:
+        is_dark_theme = True
+    else:
+        is_dark_theme = False
+    assert is_dark_theme is True
+
+def test_dark_theme_by_time_and_user_choice_v2():
+
+    current_time = time(hour=23)
+    dark_theme_enabled_by_user = None
 
     if dark_theme_enabled_by_user:
         is_dark_theme = True
@@ -33,7 +46,6 @@ def test_dark_theme_by_time_and_user_choice():
     else:
         is_dark_theme = True
     assert is_dark_theme is True
-
 
 
 def test_find_suitable_user():
